61d072377b94a8f83ea6cdbf6a32e9372e1e3855
yangtao <497067373@qq.comm>
2017-06-24 17:40:22 +0800

    add

---
+++ 进阶十二：AJAX/ajax/router.js
@@ -0,0 +1,11 @@
+// 服务端 router.js
+
+app.get('/loadMore', function(reg,res){
+	var curIdx = req.query.index
+	var len = req.query.length
+	var data =[]
+	for(var i =0; i<len; i++){
+		data.push('新闻'+(parseInt(curIdx) +i))
+	}
+	res.send(data);
+});
